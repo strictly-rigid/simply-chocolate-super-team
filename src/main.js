f0971627ac8a1247570ba95109ebc339a1897b2f
MarinaSigida <sigida.web@gmail.com>
2023-03-20 19:49:33 +0100

    Modals_version1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
-import './js/buy-now-modal';
-import './js/subscribe-modal';
+// import './js/buy-now-modal';
+// import './js/subscribe-modal';
 // import './js/reviews-slider.js';
+import './js/modals_Sihida';
+
